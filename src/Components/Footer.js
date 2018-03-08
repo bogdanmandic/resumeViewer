52f798328abf3add729643de218bf9648e7fcbc9
bogdanmandic <bogdan.mandic@riitech.com>
2018-03-08 15:58:51 +0100

    Resume Viewer by Brad Traversy

---
+++ src/Components/Footer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
+              <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i className="fa fa-google-plus"></i></a></li>
+              <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+              <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+              <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+              <li><a href="#"><i className="fa fa-skype"></i></a></li>
+            </ul>
+            <ul className="copyright">
+              <li>&copy; Copyright {new Date().getUTCFullYear()} RiiTech Solutions</li>
+              <li>Design by <a title="RiiTech Solutions" href="http://www.riitech.com/" target="_blank">RiiTech Solutions</a></li>
+            </ul>
+          </div>
+
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
